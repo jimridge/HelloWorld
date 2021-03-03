6918e80d70409b0b79d7152b0f05a5d6748fbb21
Jim Ridge <jimridge@gmail.com>
2021-03-03 17:07:19 +1100

    Modified the tilemap and player code

--- Player.gd
+++ Player.gd
@@ -2,16 +2,19 @@ extends KinematicBody2D
 
 
 # stats
+
 var score : int = 0
 var health : int = 3
- 
+
 # physics
+
+var jump_count = 0
+
 var speed : int = 200
 var jumpForce : int = 300 # 600 original value # 300 seems about right
-var jump_count : int = 0
 var max_jumps : int = 1
 var gravity : int = 1200 # 800 original value
- 
+
 var vel : Vector2 = Vector2()
 var grounded : bool = false
 
@@ -25,6 +28,8 @@ func _ready():
 	pass
 
 func _physics_process (delta):
+
+
 	# reset horizontal velocity
 	vel.x = 0
 	# movement inputs
@@ -35,18 +40,18 @@ func _physics_process (delta):
 
 	# applying the velocity
 	vel = move_and_slide(vel, Vector2.UP)
-	
+
 	# gravity
 	vel.y += gravity * delta
-	 
+
 	# jump input
 	if Input.is_action_just_pressed("jump"):
 		$AnimationPlayer.play("jump")
 		if is_on_floor():
 			vel.y -= jumpForce
 			jump_count = 0
-			
-			
+
+
 		elif !is_on_floor() and jump_count < max_jumps:
 			vel.y -= jumpForce
 			jump_count += 1
@@ -60,13 +65,13 @@ func _physics_process (delta):
 		$AnimationPlayer.play("run_right")
 	elif vel.x == 0:
 		$AnimationPlayer.play("idle")
-	
-	
-	
 
 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+
+
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+##func _process(delta):
+##	pass
